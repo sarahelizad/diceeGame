cf4be55ddf14eb50727e565b211782dab275e934
Sarah Duval <114681028+sarahelizad@users.noreply.github.com>
2022-12-18 21:19:03 -0500

    Update index.js
    Allowing changes to h1 upon page refresh. Initial navigation displays "Refresh Me" h1.

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-// Place var randomNumber1 and var randomNumber2 in a string using concatenation
-
-document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
-
-document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
-
-// if randomNumber1 > randomNumber2
-// else if randomNumber 2 > randomNumber1
-// else randomNumber1 === randomNumber2 - Draw
-// Within the curly braces - We want to use JS to change the H1
-
-
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+if (performance.getEntriesByType("navigation")[0].type === "reload") {
+    rollDice();
 }
-else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!";
+
+function rollDice() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+  document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+  changeTitle(randomNumber1, randomNumber2)
 }
-else {
-  document.querySelector("h1").innerHTML = "🚩 Draw!";
+
+function changeTitle(num1, num2) {
+  if (num1 === num2) {
+    document.querySelector("h1").innerHTML = "🚩 Draw!";
+  } else if (num1 > num2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  } else {
+    document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!";
+  }
 }
